1a291cf0a5918f188cf799d031feadb2266a9cde
david vidal <davidvidal44@gmail.com>
2023-07-23 14:38:54 -0700

    JS update tile layer

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ buildMap() {
     center: this.coordinates,
     zoom: 11,
     });
-    // add openstreetmap tiles
+    // Tile Openstreetmap ? where did it come from?
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     minZoom: '15',
     }).addTo(this.map)
-    // create and add geolocation marker
+    // 
     const marker = L.marker(this.coordinates)
     marker
     .addTo(this.map)
